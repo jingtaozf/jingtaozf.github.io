9618468ba9577c3c7dd68f5aeeebcd0ed829c39b
Jingtao Xu <jingtaozf@gmail.com>
2019-05-23 12:41:37 +0800

    change web template from muse to org mode.

---
+++ touch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ./assembly
+touch -d 20031209 Nasm.org 
+touch -d 20031027 Nasm-ds.org 
+touch -d 20020206 subject2001.org 
+touch -d 20031126 vim.org 
+touch -d 20020112 assembly.org 
+cd ../lisp
+touch -d 20131026 mixlang-in-lisp.org 
+cd ../emacs
+touch -d 20140212 TheWayIUseEmacs.org 
